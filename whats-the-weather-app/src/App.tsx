72803c8e2c6830a50d4deb574ef337eedc0a0953
Kyle Macleod <kds.gametech@gmail.com>
2026-07-08 22:39:13 +0200

    feat/0001 - Build base app layout and TODO Additions for structured plan
    Add shell structure and adjust base flex layout

--- whats-the-weather-app/src/App.tsx
+++ whats-the-weather-app/src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div id="app">
-      <h1>What's the Weather</h1>
+      <header>
+        <h1>What's the Weather</h1>
+        {/* TODO: Search input */}
+      </header>
+      <main>{/* TODO: Current weather and selected weather display */}</main>
+      <section>{/* TODO: Forecast and history */}</section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
